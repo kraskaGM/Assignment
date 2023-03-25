019cc1128c9281c42dc9f5c616b7a2a5cf266014
kraskaGM <martin.grosberg@gmail.com>
2023-03-25 17:29:19 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <adder.h>
+
+float add(float a,float b);
+int main()
+{
+    std::cout << "Hey, how are you? \n";
+
+    std::cout << add(72.1f, 33.4f) << '\n';
+
+    return 0;
+}
